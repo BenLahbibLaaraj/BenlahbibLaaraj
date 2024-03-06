9384e1e7b00d603a1a7c5518933d22c4ea94e5ad
BenLahbibLaaraj <benlahbib.laaraj@student.vives.be>
2024-03-06 15:26:58 +0100

    webb

--- Javascript/Labo 4/Man van An/scripts/code.js
+++ Javascript/Labo 4/Man van An/scripts/code.js
@@ -1,12 +1,12 @@
 const setup = () => {
-    let origineleString = "De man van An geeft geen hand aan ambetante verwanten";
+    let String = "De man van An geeft geen hand aan ambetante verwanten";
 
     let i = 0;
     let count = 0;
 
 
     while (i !== -1) {
-        i = origineleString.toLowerCase().indexOf("an", i);
+        i = String.toLowerCase().indexOf("an", i);
         if (i !== -1) {
             count++;
             i += 2;
@@ -17,11 +17,11 @@ const setup = () => {
 
 
     count = 0;
-    i = origineleString.toLowerCase().lastIndexOf("an");
+    i = String.toLowerCase().lastIndexOf("an");
 
     while (i !== -1) {
         count++;
-        i = origineleString.toLowerCase().lastIndexOf("an", i - 1);
+        i = String.toLowerCase().lastIndexOf("an", i - 1);
     }
 
     console.log(count);
